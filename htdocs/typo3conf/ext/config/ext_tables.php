7add93214154bea787670cd8091e9a406c0238e7
Martin Hummer <ma.hummer@gmail.com>
2016-11-12 22:20:20 +0100

    remove call to SpriteManager::class

--- htdocs/typo3conf/ext/config/ext_tables.php
+++ htdocs/typo3conf/ext/config/ext_tables.php
@@ -8,21 +8,4 @@ if (!defined('TYPO3_MODE')) {
 	 */
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Config');
 
-	// New icons for the BE
-	if (TYPO3_MODE === 'BE') {
-
-	    $icons = array('category', 'comment', 'storage', 'news', 'people');
-	    foreach ($icons as $icon) {
-
-	        \TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
-	            'pages',
-	            'contains-' . $icon,
-	            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('config') . 'Resources/Public/Backend/Icons/' . $icon . '.png');
-
-	        $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = array(
-	            ucfirst($icon),
-	            $icon,
-	            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('config') . 'Resources/Public/Backend/Icons/' . $icon . '.png'
-	        );
-	    }
-	}
+	
